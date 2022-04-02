18cbe49374420281c333f1b5d75ac9ed66e5d90f
JImMY5232 <98219440+JImMY5232@users.noreply.github.com>
2022-04-01 22:03:24 -0400

    Update pastebin.py

--- pastebin.py
+++ pastebin.py
@@ -1,10 +1,15 @@
 import requests
+from pprint import pprint
 
-resp_msg = requests.head('https://jsonplaceholder.typicode.com')
+resp_msg = requests.get("https://jsonplaceholder.typicode.com/user/10")
 
 if resp_msg.status_code == 200:
-  print('Response:',resp_msg.status, '🎉🎉🎉', '\n')
+  print("Response:",resp_msg.status_code, '🎉🎉🎉', '\n')
 else:
-  print('Uh Oh, got',resp_msg.status)
+  print("Uh Oh, got",resp_msg.status_code)
 
-print(resp_msg.headers)
+dict = resp_msg.json()
+
+print("name")
+print("Email:")
+print("city:")
